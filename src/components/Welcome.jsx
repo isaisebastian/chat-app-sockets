dd71df02967f3fd3cabc2ba6fe6f4309f7ddf198
isaisebastian <sebastianisai14@outlook.com>
2022-08-11 00:03:05 +0200

    chatinput & messages feature

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import Robot from "../assets/robot.gif";
+
+function Welcome({ currentUser }) {
+    return (
+        <Container>
+            <img src={Robot} alt="robot" />
+            <h1>
+                Welcome, <span>{currentUser.username}</span>!
+            </h1>
+            <h3>Please select a chat to Start Messaging.</h3>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    color: white;
+    img {
+        height: 20rem;
+    }
+
+`
+
+export default Welcome;
